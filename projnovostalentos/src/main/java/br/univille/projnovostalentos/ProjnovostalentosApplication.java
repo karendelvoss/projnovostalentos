03925fd961dbe2f38cc37fbb97473f08aea7ea18
karen.delvoss <karen.delvoss.ribas@gmail.com>
2022-12-04 11:31:42 -0300

    first commit

---
+++ projnovostalentos/src/main/java/br/univille/projnovostalentos/ProjnovostalentosApplication.java
@@ -0,0 +1,13 @@
+package br.univille.projnovostalentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjnovostalentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjnovostalentosApplication.class, args);
+	}
+
+}
---
+++ projnovostalentos/src/test/java/br/univille/projnovostalentos/ProjnovostalentosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.projnovostalentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjnovostalentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
